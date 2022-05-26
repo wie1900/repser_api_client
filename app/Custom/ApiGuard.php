5d4392e081df5fcc04d5459cca47b880b17ea0ab
wie1900 <jurek@slavis.net>
2022-05-26 17:52:35 +0200

    ApiGuard

--- app/Custom/ApiGuard.php
+++ app/Custom/ApiGuard.php
@@ -10,6 +10,7 @@ class ApiGuard
     public function __construct(string $end)
     {
         $this->endpoint = 'localhost:8001/api/'.$end;
+        // $this->endpoint = 'https://api.deadygo.com/api/'.$end;
         $this->key = 'Bearer 22|YcA7x2NY33vESYl73wmKmrW6iyrUYI1AM5Fow0aQ';
     }
 
